a2d5a5cbca6868136ecfd2b36c16f07d80206fdb
Joe <josephc.lambert@gmail.com>
2019-10-01 15:30:20 -0700

    converst app.js to class. adds axios package. stubs out html. adds initial state.

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// convert to class
+// cDM --> get first user and add to user {}
+// cDU --> get user.followers and add to followers []
+
+// components
+// UsersList
+// UserCard
+
+const d = new Date();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUserName: 'losephjambert',
+      user: {},
+      followers: [],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <header>header</header>
+        <main>main</main>
+        <footer>
+          <p>&copy; {d.getFullYear()} Joseph Lambert</p>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
